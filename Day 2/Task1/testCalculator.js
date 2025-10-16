dbc4990e46bfd624e74ef145144e5bd944a7e66a
AdityaPawar6174 <adityabpawar393@gmail.com>
2025-10-16 19:38:37 +0530

    feat: added Task1 of Day 2

---
+++ Day 2/Task1/testCalculator.js
@@ -0,0 +1,7 @@
+
+const calulator = require('./calculator');
+
+console.log(calulator.add(2, 3));
+console.log(calulator.subtract(5, 2));
+console.log(calulator.multiply(3, 4));
+console.log(calulator.divide(10, 2));
