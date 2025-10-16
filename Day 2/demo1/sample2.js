b4cf35478b3912abc77997df6fc9a6602c7d7425
AdityaPawar6174 <adityabpawar393@gmail.com>
2025-10-16 19:39:52 +0530

    feat: added demo files to clear the concept of local modules in Day 2

---
+++ Day 2/demo1/sample2.js
@@ -0,0 +1,13 @@
+function double(num){
+    return num * 2;
+}
+
+function triple(num){
+    return num * 3;
+}
+
+function square(num){
+    return num * num;
+}
+
+module.exports = {double, triple, square};
---
+++ Day 2/demo2/test.js
@@ -0,0 +1,8 @@
+
+const math = require('../demo1/sample2');
+
+console.log(
+    math.double(2),
+    math.triple(3),
+    math.square(4)
+);
